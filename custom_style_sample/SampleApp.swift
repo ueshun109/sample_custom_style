30ea471d4c51fe307b49105757f5ece904a1e41a
ueshun109 <ush109rock@gmail.com>
2023-11-19 22:16:29 +0900

    :tada: Initial commit

---
+++ custom_style_sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  custom_style_sampleApp.swift
+//  custom_style_sample
+//
+//  Created by shun uematsu on 2023/11/19.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
